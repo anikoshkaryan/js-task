af3a2d83bb0ecb887f507dd4163f8f6f4caaeefe
Kim <kimtorosyan@gmail.com>
2020-06-04 14:37:54 +0400

    added warning-message design,countries list

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ $(document).ready(function () {
 
     });
     $(".btn").click(function () {
-        let inputs = $(this).closest(".main-content").find(".input-field");
+        let inputs = $(this).closest(".main-content").find(".input-field:not(.optional)");
+        $(inputs).removeClass("invalid");
+        $(".warning-message.show").removeClass("show");
         $(inputs).each(function () {
+            console.log($(this), $(this).val());
             if ($(this).val().length === 0) {
                 $(this).addClass("invalid");
             }
@@ -19,8 +22,24 @@ $(document).ready(function () {
         $(message).text("Please fill out this field");
         $(message).addClass("show");
 
+    });
+    $.ajax({
+        url:"https://restcountries.eu/rest/v2/all",
+        success: function (result) {
+        setCountries(result);
+        }
 
     });
+    function setCountries(x) {
+        let select = $(".country-input");
+       x.forEach((country) => {
+           let option = document.createElement("option");
+          $(option).val(country.name);
+          $(option).text(country.name);
+           select.append(option);
+
+       })
+    }
 });
 
 
