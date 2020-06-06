cd929231e89f4bd690fb861a017dca4e795e3193
Kim <kimtorosyan@gmail.com>
2020-06-06 15:52:52 +0400

    added geolocation,inputmasks

--- script.js
+++ script.js
@@ -18,28 +18,72 @@ $(document).ready(function () {
 
         });
 
-        let message = $(".invalid").eq(0).closest(".input-container").find(".warning-message");
+        let message = $(this).closest(".main-content").find(".invalid").eq(0).closest(".input-container").find(".warning-message");
         $(message).text("Please fill out this field");
         $(message).addClass("show");
 
+
+    });
+
+    $(".input-field").focus(function () {
+        $(this).removeClass("invalid");
+        $(this).closest(".input-container").find(".warning-message").removeClass("show");
     });
     $.ajax({
-        url:"https://restcountries.eu/rest/v2/all",
+        url: "https://restcountries.eu/rest/v2/all",
         success: function (result) {
-        setCountries(result);
+            setCountries(result);
         }
 
     });
+
     function setCountries(x) {
         let select = $(".country-input");
-       x.forEach((country) => {
-           let option = document.createElement("option");
-          $(option).val(country.name);
-          $(option).text(country.name);
-           select.append(option);
+        x.forEach((country) => {
+            let option = document.createElement("option");
+            $(option).val(country.name);
+            $(option).text(country.name);
+            select.append(option);
 
-       })
+        })
     }
+
+    $(".geolocation-icon").click(function () {
+        if (!navigator.geolocation) {
+            alert('Geolocation is not supported by your browser');
+        } else {
+            navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
+        }
+    });
+
+    function locationSuccess(position) {
+        let lat = position.coords.latitude;
+        let long = position.coords.longitude;
+        $.ajax({
+            url: `https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&key=85c6959e8b7e40beab666006dcf5d35d`,
+            success: function (result) {
+                let city = result.results[0].components.city;
+                let country = result.results[0].components.country;
+                $("[name= 'city']").val(city);
+                $(`.country-input option[value='${country}']`).attr("selected", "true");
+                $(".country-input").removeClass("invalid");
+                $("[name= 'city']").removeClass("invalid");
+            }
+        })
+    }
+
+    function locationFail() {
+        alert('Unable to retrieve your location');
+    }
+
+    $(".input-phone").inputmask("+(374)99 99-99-99");
+    $("[name='email-address']").inputmask({alias: "email"});
+    $("[name='card-number']").inputmask("9999 9999 9999 9999");
+    $(".date").inputmask({
+        alias: 'datetime',
+        inputFormat: 'mm/yy'
+    });
+    $(".code").inputmask("999");
 });
 
 
