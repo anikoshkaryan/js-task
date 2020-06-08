ac8c4eb35155100e65368b3abc8135703086a5da
Kim <kimtorosyan@gmail.com>
2020-06-08 14:30:01 +0400

    added final improvements

--- script.js
+++ script.js
@@ -1,30 +1,58 @@
 /* order-stezps function*/
 $(document).ready(function () {
-    $(".step-title").click(function () {
+    /*$(".step-title").click(function () {
         let form = $(this).attr("data-form");
         $(" .form-container > div").addClass("hidden");
         $(`.${form}`).removeClass("hidden");
 
-    });
+    });*/
     $(".btn").click(function () {
         let inputs = $(this).closest(".main-content").find(".input-field:not(.optional)");
         $(inputs).removeClass("invalid");
         $(".warning-message.show").removeClass("show");
         $(inputs).each(function () {
-            console.log($(this), $(this).val());
             if ($(this).val().length === 0) {
                 $(this).addClass("invalid");
             }
 
         });
+        let invalid = $(this).closest(".main-content").find(".invalid");
+        if (invalid.length === 0) {
+            let active = $("[data-next]:not('.hidden')");
+            let activeAttribute = $(active).attr("data-next");
+            $(`.${activeAttribute}`).removeClass("hidden");
+            if (activeAttribute === "thanks-message") {
+                $(".order-steps").addClass("hidden");
+                thanksBlock();
+            } else {
+                $(".step-title").removeClass("active");
+                $(`[data-form=${activeAttribute}]`).addClass("active");
+                $(active).addClass("hidden");
+            }
 
-        let message = $(this).closest(".main-content").find(".invalid").eq(0).closest(".input-container").find(".warning-message");
-        $(message).text("Please fill out this field");
-        $(message).addClass("show");
+
+        } else {
+            let message = $(invalid).eq(0).closest(".input-container").find(".warning-message");
+            $(message).text("Please fill out this field");
+            $(message).addClass("show");
+        }
 
 
     });
-
+    function thanksBlock (){
+        $(".thanks-message .email").text($("[name='email-address']").val());
+        let num = localStorage.getItem("orderNumber");
+        if (!num){
+            localStorage.setItem("orderNumber",188787788);
+        }else {
+            localStorage.setItem("orderNumber", +num + 1);
+        }
+        $(".ordNum").text(localStorage.getItem("orderNumber"));
+        let date = $(".delivery-date");
+        let now = new  Date();
+        now.setMonth(now.getMonth() + 1);
+        date.text(now);
+    }
     $(".input-field").focus(function () {
         $(this).removeClass("invalid");
         $(this).closest(".input-container").find(".warning-message").removeClass("show");
@@ -62,12 +90,13 @@ $(document).ready(function () {
         $.ajax({
             url: `https://api.opencagedata.com/geocode/v1/json?q=${lat},${long}&key=85c6959e8b7e40beab666006dcf5d35d`,
             success: function (result) {
+                let active = $("[data-next]:not('.hidden')");
                 let city = result.results[0].components.city;
                 let country = result.results[0].components.country;
-                $("[name= 'city']").val(city);
-                $(`.country-input option[value='${country}']`).attr("selected", "true");
-                $(".country-input").removeClass("invalid");
-                $("[name= 'city']").removeClass("invalid");
+                $(active).find("[name='city']").val(city);
+                $(active).find(`.country-input option[value='${country}']`).attr("selected", "true");
+                $(active).find(".country-input").removeClass("invalid");
+                $(active).find("[name= 'city']").removeClass("invalid");
             }
         })
     }
@@ -84,6 +113,19 @@ $(document).ready(function () {
         inputFormat: 'mm/yy'
     });
     $(".code").inputmask("999");
+    $(".same-as-shipping").click(function () {
+        let $copyInputs = [...document.querySelectorAll(".copy")];
+        let $pasteInputs = [...document.querySelectorAll(".paste")];
+        $copyInputs.forEach(function (copyInput) {
+            let filtered = $pasteInputs.filter(function (pasteInput) {
+                return $(pasteInput).attr("name") === $(copyInput).attr("name");
+            });
+            if (filtered.length > 0) {
+                filtered[0].value = copyInput.value;
+            }
+
+        });
+    })
 });
 
 
